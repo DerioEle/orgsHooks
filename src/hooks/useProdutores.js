9c663dab564a100c7604be7508e962243b189d02
Dério Oliveira <dco.ele21@uea.edu.br>
2023-05-16 12:02:12 -0400

    Versão Final

--- src/hooks/useProdutores.js
+++ src/hooks/useProdutores.js
@@ -2,14 +2,24 @@ import { useState, useEffect } from "react";
 
 import { carregaProdutores } from "../servicos/carregaDados";
 
+const ordernarLista = (a, b) => {
+    if (a.distancia < b.distancia)
+        return -1;
+    if (a.distancia > b.distancia)
+        return 1;
+    return 0;
+}
+
 export default function useProdutores(){
     const [titulo, setTitulo] = useState("");
     const [lista, setLista] = useState([]);
-
+    
     useEffect(() =>{
         const retorno = carregaProdutores();
+        retorno.lista.sort(ordernarLista);
+        
         setTitulo(retorno.titulo);
-        setLista(retorno.lista);
+        setLista(retorno.lista); 
     }, [])
 
     return [titulo, lista];
--- src/telas/Home/componentes/Produtor.js
+++ src/telas/Home/componentes/Produtor.js
@@ -1,13 +1,29 @@
-import React, { useState } from 'react';
+import React, { useReducer, useMemo } from 'react';
 import { Image, StyleSheet, Text, View, TouchableOpacity } from 'react-native';
 
 import Estrelas from '../../../componentes/Estrelas';
 
+const distanciaEmMetros = (distancia) => {
+    console.log("distanciaEmMetros");
+    return `${distancia}m`;
+}
+
 export default function Produtor({ nome, imagem, distancia, estrelas }){
-   const [selecionado, setSelecionado] = useState(false);
+   //const [selecionado, setSelecionado] = useState(false);
+
+   const [selecionado, inverterSelecionado] = useReducer(
+        (selecionado)=> !selecionado,
+        false
+   );
+
+   const distanciaTexto = useMemo(
+        () => distanciaEmMetros(distancia), 
+        [distancia]
+    );
+
     return <TouchableOpacity 
     style={estilos.cartao}
-    onPress={()=> setSelecionado(!selecionado)}
+    onPress={inverterSelecionado}
     >
         <Image source={imagem} accessibilityLabel={nome} style ={estilos.imagem}/>
         <View style={estilos.informacoes}>
@@ -19,7 +35,7 @@ export default function Produtor({ nome, imagem, distancia, estrelas }){
                 grande={selecionado}
                 />
             </View>
-            <Text style={estilos.distancia}  >{ distancia }</Text>
+            <Text style={estilos.distancia}  >{ distanciaTexto }</Text>
         </View>
         
     </TouchableOpacity>
